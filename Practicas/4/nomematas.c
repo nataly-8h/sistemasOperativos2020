031d5e10a81a2b3727beeb24dee8833e2c495416
nataly-8h <natalyhn.08@gmail.com>
2020-09-30 00:28:47 +0000

    Practica 4

--- Practicas/4/nomematas.c
+++ Practicas/4/nomematas.c
@@ -2,12 +2,13 @@
 #include <signal.h>
 #include <unistd.h>
 
+
 void signalHandler(int signum){
     printf("haha no me matas \n");
 }
 
 int main(){
-    signal(2, signalHandler);
+    signal(17, signalHandler);
     while(1){
         sleep(2);
         printf("trabajando \n");
