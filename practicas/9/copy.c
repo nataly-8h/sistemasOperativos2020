94bb50b2e392f2d8fc84f28756cffd4834460d50
nataly-8h <natalyhn.08@gmail.com>
2020-11-11 03:21:53 +0000

    Practica 9

---
+++ practicas/9/copy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char **argv){
+    char *sourceFile = argv[1];
+    char *destinationFile = argv[2];
+
+    int fd = open(sourceFile, O_RDONLY, 0); // abrir archivos, fopen
+    if(fd < 0){
+        printf("Error al abrir archivo \n");
+        return 1;
+    }
+     int dFd = open(destinationFile, O_WRONLY|O_CREAT, 0600);
+    char c;
+    while(read(fd, &c, 1)){
+        write(dFd, &c, 1);
+    }
+    close(fd);
+    close(dFd);
+    return 0;
+}
